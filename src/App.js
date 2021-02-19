52b8af1c671a53d855d9ee4715c99628a17d5251
Шмаков Сергей Владимирович <Shmakov.S.Vlad@sberbank.ru>
2021-02-19 16:37:48 +0300

    Диплинки на продукту ПИФ и ДУ

--- src/App.js
+++ src/App.js
@@ -4,44 +4,103 @@ import React from "react";
 const platform = {
   android_prom: {
     name: "Android ПРОМ",
-    base: "android-app://ru.sberbankmobile/android-app/ru.sberbankmobile/pfm/marketplace",
-    marketplaceId: true,
+    base: "android-app://ru.sberbankmobile/android-app/ru.sberbankmobile/",
+    appendCode: true,
     ext: "mobile"
   },
   android_test: {
     name: "Android ТЕСТ",
-    base: "android-app://ru.sberbankmobile_alpha/android-app/ru.sberbankmobile_alpha/pfm/marketplace",
-    marketplaceId: true,
+    base: "android-app://ru.sberbankmobile_alpha/android-app/ru.sberbankmobile_alpha/",
+    appendCode: true,
     ext: "mobile"
   },
   ios: {
     name: "iOS",
-    base: "sberbankonline://pfm/marketplace",
-    marketplaceId: true,
+    base: "sberbankonline://",
+    appendCode: true,
     ext: "mobile"
   },
   smart: {
-    name: "Смартлинк",
-    base: "https://sberbank.ru/sms/pfmmp-website",
-    marketplaceId: false,
+    name: "Смартлинк общий",
+    base: "https://sberbank.ru/sms/",
+    appendCode: false,
     ext: "smart"
   },
   smart_id: {
-    name: "Смартлинк на подвитрину",
-    base: "https://sberbank.ru/sms/pfmmp-website",
-    marketplaceId: true,
+    name: "Смартлинк с указанием экрана",
+    base: "https://sberbank.ru/sms/",
+    appendCode: true,
     ext: "smart"
   }
 };
 
-const marketplace = [
-  {code: "investmentsCatalog", name: "Основная витрина инвестиций"},
-  {code: "PIFCatalog",         name: "Витрина категории ПИФ"},
-  {code: "IIS_products",       name: "Витрина ИИС с продуктами"},
-  {code: "trustCatalog",       name: "Доверительное управление"},
-  {code: "investPlansAllMB",   name: "Инвестиционные планы"},
-  {code: "forYouCatalog",      name: "По уровню риска"},
+const deeplinks = {
+  marketplace: {
+    android_prom: {link: "pfm/marketplace", param: "marketPlaceId"},
+    android_test: {link: "pfm/marketplace", param: "marketPlaceId"},
+    ios: {link: "pfm/marketplace", param: "marketPlaceId"},
+    smart: {link: "pfmmp-website", param: ""},
+    smart_id: {link: "mp_all", param: ""}
+  },
+  pif: {
+    android_prom: {link: "welfare/invest-pif/product", param: "productCode"},
+    android_test: {link: "welfare/invest-pif/product", param: "productCode"},
+    ios: {link: "welfare/invest-pif/product", param: "productCode"},
+    smart: {link: "fund_all", param: ""},
+    smart_id: {link: "fund_all", param: ""}
+  },
+  trust: {
+    android_prom: {link: "investments/tm/product/card", param: "productCode"},
+    android_test: {link: "investments/tm/product/card", param: "productCode"},
+    ios: {link: "investments/tm/product/card", param: "productCode"},
+    smart: {link: "iia_saving-website", param: ""},
+    smart_id: {link: "iia_all", param: ""}
+  }
+};
+
+const targets = [
+  {
+    code: "marketplace",    name: "Витрина", param: "marketPlaceId",
+    options: [
+      {code: "investmentsCatalog", name: "Основная витрина инвестиций"},
+      {code: "PIFCatalog",         name: "Витрина категории ПИФ"},
+      {code: "IIS_products",       name: "Витрина ИИС с продуктами"},
+      {code: "trustCatalog",       name: "Доверительное управление"},
+      {code: "investPlansAllMB",   name: "Инвестиционные планы"},
+      {code: "forYouCatalog",      name: "По уровню риска"},
+    ]
+  },
+  {
+    code: "pif",            name: "Продукты ПИФ",     param: "productCode",
+    options: [
+      {code: "fund_equity_dn",   name: "Фонд акций Добрыня Никитич"},
+      {code: "fund_bond_im",     name: "Фонд облигаций Илья Муромец"},
+      {code: "fund_balanced",    name: "Фонд Сбалансированный"},
+      {code: "fund_prosp_bonds", name: "Фонд перспективных облигаций"},
+      {code: "fund_eurobonds",   name: "Фонд Еврооблигации"},
+      {code: "fund_debt_market", name: "Фонд Глобальный долговой рынок"},
+      {code: "fund_money",       name: "Фонд Денежный"}
+    ]
+  },
+  {
+    code: "trust",          name: "Продукты ДУ",      param: "productCode",
+    options: [
+      {code: "trust_conservative", name: "Стратегия \"Консервативный портфель\""},
+      {code: "trust_careful", name: "Стратегия \"Осторожный портфель\""},
+      {code: "trust_moderate", name: "Стратегия \"Умеренный портфель\""},
+      {code: "trust_modagressive", name: "Стратегия \"Умеренно-агрессивный портфель\""},
+    ]
+  }
 ];
+
+// const marketplace = [
+//   {code: "investmentsCatalog", name: "Основная витрина инвестиций"},
+//   {code: "PIFCatalog",         name: "Витрина категории ПИФ"},
+//   {code: "IIS_products",       name: "Витрина ИИС с продуктами"},
+//   {code: "trustCatalog",       name: "Доверительное управление"},
+//   {code: "investPlansAllMB",   name: "Инвестиционные планы"},
+//   {code: "forYouCatalog",      name: "По уровню риска"},
+// ];
 
 const externalSources = [
   {code: "",             name: "Внутренний",     source: "", medium: ""},
@@ -69,7 +128,8 @@ const internalSources = [
 class App extends React.Component {
   state = {
     platformCode: "android_prom",
-    marketplace: marketplace[0].code,
+    target: "marketplace",
+    targetCode: targets[0].options[0].code,
     internal: internalSources[0].code,
     external: externalSources[0].code,
     campaign: "",
@@ -77,12 +137,38 @@ class App extends React.Component {
     debug: ""
   };
 
+  getTargetOptions = () => {
+    let t = targets.find((element) => {
+      return element.code === this.state.target;
+    });
+    if(t === undefined)
+      return [];
+    return t.options;
+  };
+
+  getTargetParam = (symbol) => {
+    let t = targets.find((element) => {
+      return element.code === this.state.target;
+    });
+    if((t === undefined) || (t === ""))
+      return "";
+    return t.param + symbol;
+  };
+
+
   getLink = () => {
     let platform_record = platform[this.state.platformCode];
     let link = platform_record.base;
+    let deeplink = deeplinks[this.state.target][this.state.platformCode];
+    if(deeplink !== undefined)
+      link = link + deeplink.link;
     let params = [];
-    if(platform_record.marketplaceId)
-      params.push("marketPlaceId=" + this.state.marketplace);
+    if(platform_record.appendCode) {
+      if(deeplink.param !== "")
+        params.push(deeplink.param + "=" + this.state.targetCode);
+      else
+        params.push(this.state.targetCode);
+    }
     if(this.state.internal !== "")
       params.push("internal_source=" + this.state.internal);
     if(this.state.external !== "") {
@@ -127,8 +213,15 @@ class App extends React.Component {
     // document.getElementById("debug").innerText = this.getLink();
   };
 
-  onMplaceChanged = (e) => {
-    this.setState({marketplace: e.currentTarget.value});
+  onTargetChanged = (e) => {
+    this.setState({
+      target: e.currentTarget.value
+    });
+    // document.getElementById("debug").innerText = this.getLink();
+  };
+
+  onCodeChanged = (e) => {
+    this.setState({targetCode: e.currentTarget.value});
     // document.getElementById("debug").innerText = this.getLink();
   };
 
@@ -157,7 +250,7 @@ class App extends React.Component {
           <table>
             <tbody>
             <tr>
-              <th>Платформа</th><th>Витрина</th><th>Внутренняя точка перехода</th><th>Рассылка</th>
+              <th>Платформа</th><th>Цель</th><th>Экран</th><th>Внутренняя точка перехода</th><th>Рассылка</th>
             </tr>
             <tr>
               <td className="App-cell">{Object.keys(platform).map((k) => {
@@ -177,15 +270,29 @@ class App extends React.Component {
                   // return <option value={platform[k].base}>{platform[k].name}</option>;
                 })}
               </td>
-              <td className="App-cell">{marketplace.map((k) => {
-                  let id = "mplace" + k.code;
+              <td className="App-cell">{targets.map((k) => {
+                  let id = "target_" + k.code;
+                  return (
+                      <div className="App-select">
+                        <input type="radio" name="target" id={id}
+                               value={k.code}
+                               onChange={this.onTargetChanged}
+                               checked={this.state.target === k.code}
+
+                        />
+                        <label for={id}>{k.name}</label>
+                      </div>
+                  );
+                })}
+              </td>
+              <td className="App-cell">{this.getTargetOptions().map((k) => {
+                  let id = "targetCode_" + k.code;
                   return (
                       <div className="App-select">
                         <input type="radio" name="marketplace" id={id}
                                value={k.code}
-                               onChange={this.onMplaceChanged}
-                               checked={this.state.marketplace === k.code}
-
+                               onChange={this.onCodeChanged}
+                               checked={this.state.targetCode === k.code}
                         />
                         <label for={id}>{k.name}</label>
                       </div>
